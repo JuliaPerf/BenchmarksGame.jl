c61efcb2ff2deae65d37b5b9ec64b1f557ae9486
SimonDanisch <sdanisch@gmail.com>
2019-02-18 14:15:39 +0100

    fence windows fix

--- jl_build/make.jl
+++ jl_build/make.jl
@@ -13,12 +13,17 @@ benchmarks = [
     ("spectralnorm"),
 ]
 
-i = raw"C:\Users\sdani\.julia\dev\WinRPM\deps\usr\x86_64-w64-mingw32\sys-root\mingw\include"
+cc_flags = String[]
+if iswindows()
+    using WinRPM
+    WinRPM.installdir
+    push!(cc_flags, joinpath(WinRPM.installdir, "usr", "$(Sys.ARCH)-w64-mingw32", "sys-root", "mingw", "include"))
+end
 
 for bench in benchmarks
   build_executable(
       joinpath(@__DIR__, "..", bench, "cmain.jl"), # Julia script containing a `julia_main` function, e.g. like `examples/hello.jl`
       builddir = joinpath(@__DIR__, bench), # that's where the compiled artifacts will end up [optional]
-      cc_flags = ["-I$i"],
+      cc_flags = cc_flags,
   )
 end
